3e32dec107eb407f1491a7825ad8a8726eddac0a
Refki Jorgi Pradana <refki895@gmail.com>
2021-12-24 10:25:05 +0700

    eleventh commit

--- resources/views/pages/repository/show.blade.php
+++ resources/views/pages/repository/show.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="container-fluid" id="container-wrapper">
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dokumen Mutu</h1>
+        <h1 class="h3 mb-0 text-gray-800">Dokumen Mutu </h1>
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
             <li class="breadcrumb-item"><a href="{{ route('dokumen-mutu.index') }}">Dokumen Mutu</a></li>
